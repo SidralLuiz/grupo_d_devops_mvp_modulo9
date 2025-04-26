21dc394485a5f548cdf44a53b804a8d5a3e4161f
Luiz Fávio Sidral <126919151+SidralLuiz@users.noreply.github.com>
2025-04-26 15:49:08 -0300

    Update main.tf

--- terraform/project/modules/database/main.tf
+++ terraform/project/modules/database/main.tf
@@ -16,7 +16,7 @@ resource "aws_instance" "backend_ec2" {
   instance_type = "t3.micro"
   ami = data.aws_ami.imagem_ec2.id
   subnet_id = var.sn_pub01
-  vpc_security_group_ids = ["sg-04f92b4f0994a1085"]
+  vpc_security_group_ids = [aws_security_group.grupo_d_.id]
   key_name =  data.aws_key_pair.lb_ssh_key_pair_grupo_d.key_name
 
   associate_public_ip_address = true

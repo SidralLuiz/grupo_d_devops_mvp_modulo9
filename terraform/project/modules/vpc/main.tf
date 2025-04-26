e03e2bfc4562c680adde0b9f2513e4f18d255cd4
Luiz Fávio Sidral <126919151+SidralLuiz@users.noreply.github.com>
2025-04-26 17:07:34 -0300

    Update main.tf

--- terraform/project/modules/vpc/main.tf
+++ terraform/project/modules/vpc/main.tf
@@ -14,9 +14,9 @@ resource "aws_subnet" "sn_pub01" {
 
 
 resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.vpc_grupo_d.id
+  vpc_id = data.aws_vpc.vpc_grupo_d.id
   tags = {
-    Name = "grupo_d-igw" 
+    Name = "grupo_d_igw" 
   }
 }
 
@@ -24,7 +24,7 @@ resource "aws_route_table" "route_pub" {
   vpc_id = data.aws_vpc.vpc_grupo_d.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.grupo_igw.id
+    gateway_id = aws_internet_gateway.grupo_d_igw.id
   }
   tags = {
     Name = "grupo_d-routetable"

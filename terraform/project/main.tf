d94a21c5b9d835334c3bd727e1fb58394b87b653
Luiz Fávio Sidral <126919151+SidralLuiz@users.noreply.github.com>
2025-04-26 16:24:56 -0300

    Update main.tf

--- terraform/project/main.tf
+++ terraform/project/main.tf
@@ -4,26 +4,26 @@ module "vpc" {
 }
 module "loadbalancer" {
     source = "./modules/loadbalancer"
-    vpc_id = data.aws_vpc.existing_vpc.id
+    vpc_id = data.aws_security_group.grupo_d_sg.id
     sn_pub01 = module.vpc.sn_pub01
 
 }
 
 module "backend" {
     source = "./modules/backend"
-    vpc_id = data.aws_vpc.existing_vpc.id
+    vpc_id = data.aws_security_group.grupo_d_sg.id
    sn_pub01 = module.vpc.sn_pub01
  
 }
 
 module "database" {
     source = "./modules/database"
-    vpc_id = data.aws_vpc.existing_vpc.id
+    vpc_id = data.aws_security_group.grupo_d_sg.id
     sn_pub01 = module.vpc.sn_pub01
 }
 
 module "frontend" {
     source = "./modules/frontend"
-    vpc_id = data.aws_vpc.existing_vpc.id
+    vpc_id = data.aws_security_group.grupo_d_sg.id
     sn_pub01 = module.vpc.sn_pub01
 }
